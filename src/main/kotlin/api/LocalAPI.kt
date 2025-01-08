bfed92051f6af4cb1b9438cec87d2a0d05c49e5c
DerGruenkohl <karlbaum100@gmail.com>
2025-01-08 01:06:21 +0100

    optimize http requests

--- src/main/kotlin/api/LocalAPI.kt
+++ src/main/kotlin/api/LocalAPI.kt
@@ -1,6 +1,7 @@
 package api
 
-import apiUrl
+import Config.apiUrl
+import Config.key
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.*
@@ -9,18 +10,21 @@ import io.ktor.client.plugins.auth.providers.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.serialization.kotlinx.json.*
-import key
+import io.ktor.util.logging.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.serialization.json.Json
 
-class LocalAPI {
+private class LocalAPI {
     val client by lazy {
         HttpClient(CIO) {
             defaultRequest {
                 url(apiUrl)
             }
-            expectSuccess = false
+            expectSuccess = true
             install(Logging)
-            install(HttpRequestRetry){
+            install(HttpRequestRetry) {
                 retryOnServerErrors(3)
                 retryOnException(3, true)
             }
@@ -42,3 +46,24 @@ class LocalAPI {
     }
 }
 
+object ApiInstance {
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(Dispatchers.IO + job)
+    private var _client: HttpClient? = null
+    private val LOGGER = KtorSimpleLogger("LocalAPIInstance")
+
+    val client: HttpClient
+        get() {
+            if (_client == null) {
+                LOGGER.info("Creating new client")
+                _client = LocalAPI().client
+            }
+            return _client!!
+        }
+
+    fun close() {
+        LOGGER.info("Closing client")
+        _client?.close()
+        job.cancel()
+    }
+}
--- src/main/kotlin/utils/utils.kt
+++ src/main/kotlin/utils/utils.kt
@@ -1,9 +1,11 @@
 package utils
 
-import api.LocalAPI
+import api.ApiInstance
+import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
 import java.io.BufferedReader
@@ -12,30 +14,19 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
+@Serializable
+data class MojangResponse(
+    val name: String,
+    val id: String
+)
+
+fun getMinecraftUsername(uuid: String): String = runBlocking {
+    ApiInstance.client.request("https://mowojang.matdoes.dev/$uuid").body<MojangResponse>().name
+    }
 
 @Throws(IOException::class)
-fun getMinecraftUsername(uuid: String): String = runBlocking{
-    val client = LocalAPI().client
-
-    val url = URL("https://sessionserver.mojang.com/session/minecraft/profile/$uuid")
-    val text = client.request(url).bodyAsText()
-    client.close()
-    return@runBlocking Json.parseToJsonElement(text).jsonObject["name"].toString().replace("\"", "")
-    //val json = JSONObject(jsonText)
-   // return@runBlocking json.getString("name")
-}
-
-@Throws(IOException::class)
-fun getMinecraftUUID(name: String): String = runBlocking{
-    val url = URL("https://api.mojang.com/users/profiles/minecraft/$name")
-    val client = LocalAPI().client
-    val text = client.request(url).bodyAsText()
-    client.close()
-    return@runBlocking Json.parseToJsonElement(text).jsonObject["id"].toString().replace("\"", "")
-   // val input = url.openConnection().getInputStream()
-    //val jsonText = IOUtils.toString(input, "UTF-8")
-    //val json = JSONObject(jsonText)
-    //return json.getString("id")
+fun getMinecraftUUID(name: String): String = runBlocking {
+    ApiInstance.client.request("https://mowojang.matdoes.dev/$name").body<MojangResponse>().id
 }
 fun <K, V> getPairsInRange(map: Map<K, V>, startIndex: Int, range: Int = 10): List<Pair<K, V>> {
     // Convert map entries to a list
