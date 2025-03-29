71a09f2eb69608325f8cdd8b96bb4d546d6a17b8
DerGruenkohl <karlbaum100@gmail.com>
2025-03-29 11:57:43 +0100

    implement caching for EliteAPI

--- src/main/kotlin/com/dergruenkohl/commands/gain/EliteRequester.kt
+++ src/main/kotlin/com/dergruenkohl/commands/gain/EliteRequester.kt
@@ -3,16 +3,36 @@ package com.dergruenkohl.commands.gain
 import com.dergruenkohl.api.client
 import com.dergruenkohl.api.hypixelClient
 import com.dergruenkohl.hypixel.client.getSelectedProfileID
+import io.github.reactivecircus.cache4k.Cache
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import kotlin.time.Duration.Companion.minutes
 
 
+private val SkillCache = Cache.Builder<Pair<String, Int>, IncomingSkills>()
+    .expireAfterWrite(60.minutes)
+    .build()
+private val CropCache = Cache.Builder<Pair<String, Int>, IncomingCollection>()
+    .expireAfterWrite(60.minutes)
+    .build()
 
 suspend fun getSkillGraph(uuid: String, days: Int): IncomingSkills? {
+    val pair = Pair(uuid, days)
+    SkillCache.get(pair)?.let {
+        return it
+    }
     val profileID = hypixelClient.getSelectedProfileID(uuid)?: return null
-    return client.request("https://api.elitebot.dev/graph/${uuid}/${profileID.replace("-", "")}/skills?days=$days&perDay=1").body()
+    val reply = client.request("https://api.elitebot.dev/graph/${uuid}/${profileID.replace("-", "")}/skills?days=$days&perDay=1").body<IncomingSkills>()
+    SkillCache.put(pair, reply)
+    return reply
 }
 suspend fun getCropGraph(uuid: String, days: Int): IncomingCollection? {
+    val pair = Pair(uuid, days)
+    CropCache.get(pair)?.let {
+        return it
+    }
     val profileID = hypixelClient.getSelectedProfileID(uuid)?: return null
-    return client.request("https://api.elitebot.dev/graph/${uuid}/${profileID.replace("-", "")}/crops?days=$days&perDay=1").body()
+    val reply = client.request("https://api.elitebot.dev/graph/${uuid}/${profileID.replace("-", "")}/crops?days=$days&perDay=1").body<IncomingCollection>()
+    CropCache.put(pair, reply)
+    return reply
 }
