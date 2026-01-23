6adf10654332298fa81a533912f4f74781a67480
DerGruenkohl <karlbaum100@gmail.com>
2026-01-23 13:24:02 +0100

    change gif creation to use ffmpeg to produce high quality gifs instead of having to rely on webp

--- src/main/kotlin/com/dergruenkohl/commands/utility/GifCommand.kt
+++ src/main/kotlin/com/dergruenkohl/commands/utility/GifCommand.kt
@@ -2,6 +2,7 @@ package com.dergruenkohl.commands.utility
 
 import com.dergruenkohl.utils.upload
 import com.sksamuel.scrimage.ImmutableImage
+import com.sksamuel.scrimage.nio.GifWriter
 import com.sksamuel.scrimage.nio.StreamingGifWriter
 import com.sksamuel.scrimage.webp.WebpWriter
 import dev.freya02.botcommands.jda.ktx.components.Container
@@ -14,8 +15,11 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.utils.FileUpload
 import org.apache.commons.io.output.ByteArrayOutputStream
+import org.bytedeco.ffmpeg.global.avutil
 import java.io.OutputStream
 import org.bytedeco.javacv.*
+import java.io.File
+import java.net.URI
 import java.net.URL
 import java.time.Duration
 
@@ -43,12 +47,12 @@ class GifCommand: ApplicationCommand() {
                 val progressBar = "█".repeat(progress / 5) + "░".repeat(20 - progress / 5)
                 event.hook.editOriginal("Converting... [$progressBar] $progress%").queue()
             }
-
-            if (file.isImage) {
-                convertImage(file, stream)
-            } else {
-                convertVideo(file, stream, progressCallback)
-            }
+            convertVideoWithRecoder(file, stream, progressCallback)
+//            if (file.isImage) {
+//                convertImage(file, stream)
+//            } else {
+//                convertVideo(file, stream, progressCallback)
+//            }
 
             val fileUpload = FileUpload.fromData(stream.toByteArray(), "meow.gif")
 //            val container = Container.of (
@@ -79,20 +83,60 @@ class GifCommand: ApplicationCommand() {
             event.hook.editOriginal("An error occurred while converting").queue()
         }
     }
+    fun convertVideoWithRecoder(attachment: Message.Attachment, stream: OutputStream, progressCallback: (Int) -> Unit = {}){
+        val file = File("images/temp/${attachment.idLong}.gif")
+        file.createNewFile()
+        try {
+            val grabber = FFmpegFrameGrabber(URI(attachment.url).toURL())
+            grabber.start()
+
+            val recorder = FFmpegFrameRecorder.createDefault(file, attachment.width, attachment.height)
+            recorder.pixelFormat = avutil.AV_PIX_FMT_RGB8
+            recorder.frameRate = grabber.frameRate // Match input frame rate
+            recorder.start()
+            val totalFramesToExtract = (grabber.lengthInFrames).coerceAtLeast(1)
+            var extractedFrames = 0
+            var lastProgress = 0
+            logger.info { "input framerate: ${grabber.frameRate}" }
+            logger.info { "input frames: ${grabber.lengthInFrames}" }
+
+            var frame = grabber.grabFrame(false, true, true, false)
+            while (frame != null) {
+                recorder.record(frame)
+                frame = grabber.grabFrame(false, true, true, false)
+                extractedFrames++
+                // Update progress every 5%
+                val progress = (extractedFrames * 100) / totalFramesToExtract
+                if (progress != lastProgress && progress % 5 == 0) {
+                    progressCallback(progress)
+                    lastProgress = progress
+                }
+            }
+            progressCallback(100)
+            recorder.stop()
+            grabber.stop()
+            val inputStream = file.inputStream()
+            inputStream.copyTo(stream)
+            inputStream.close()
+            file.delete()
+        } catch (e: Exception) {
+            logger.error(e) { "Error while converting" }
+            file.delete()
+        }
+    }
+
 
     fun convertVideo(attachment: Message.Attachment, stream: OutputStream, progressCallback: (Int) -> Unit = {}) {
-        val grabber = FFmpegFrameGrabber(URL(attachment.url))
+        val grabber = FFmpegFrameGrabber(URI(attachment.url).toURL())
         val converter = Java2DFrameConverter()
 
         try {
             grabber.start()
             val frameRate = grabber.frameRate
             val videoDuration = grabber.lengthInTime / 1_000_000.0
-            val targetFps = 15.0
-
-            val totalFramesToExtract = (videoDuration * targetFps).toInt()
-            val frameSkip = (frameRate / targetFps).toInt().coerceAtLeast(1)
-            val frameDelayMs = (1000.0 / targetFps).toLong()
+            logger.info { "Video frameRate = $frameRate, Video duration = $videoDuration" }
+            val totalFramesToExtract = (grabber.lengthInFrames).coerceAtLeast(1)
+            val frameDelayMs = (1000.0 / frameRate).toLong()
 
             val writer = StreamingGifWriter(Duration.ofMillis(frameDelayMs), true, false)
             val gif = writer.prepareStream(stream, java.awt.image.BufferedImage.TYPE_INT_RGB)
@@ -100,9 +144,8 @@ class GifCommand: ApplicationCommand() {
             var extractedFrames = 0
             var frame = grabber.grabFrame()
             var lastProgress = -1
-
-            while (frame != null && extractedFrames < totalFramesToExtract) {
-                if (frameNumber % frameSkip == 0 && frame.image != null) {
+            while (frame != null && extractedFrames <= totalFramesToExtract) {
+                if (frame.image != null) {
                     val bufferedImage = converter.convert(frame)
                     val immutableImage = ImmutableImage.fromAwt(bufferedImage)
 
@@ -125,15 +168,14 @@ class GifCommand: ApplicationCommand() {
         } finally {
             grabber.stop()
             grabber.release()
+            logger.info { "Finished converting" }
         }
     }
 
     fun convertImage(attachment: Message.Attachment, stream: OutputStream) {
-        val inputUrl = attachment.url
-        val image = ImmutableImage.loader().fromUrl(URL(inputUrl))
-
-
+        val inputUrl = attachment.proxyUrl
+        val image = ImmutableImage.loader().fromUrl(URI(inputUrl).toURL())
         val writer = WebpWriter.DEFAULT.withLossless()
-        writer.write(image, image.metadata, stream)
+        GifWriter.Progressive.write(image, image.metadata, stream)
     }
 }
